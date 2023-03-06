b8b209a415372876e1f53ffceb8aaddb839759e8
Patrick Koers <pkoers75@gmail.com>
2023-03-06 17:19:22 +0100

    Entry table and model added

---
+++ db/migrate/20230306161648_create_entries.rb
@@ -0,0 +1,12 @@
+class CreateEntries < ActiveRecord::Migration[7.0]
+  def change
+    create_table :entries do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
