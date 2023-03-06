f4b622449c167e71604c35458e50d6100b39a0f0
Patrick Koers <pkoers75@gmail.com>
2023-03-06 18:07:21 +0100

    pages/entries/index.html.erb showing all entries from the table

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,25 @@
 class EntriesController < ApplicationController
+  def index
+    @entries = Entry.all #current_user.entries
+  end
+
+  def new
+    @entry = Entry.new
+  end
+
+  def create
+    @entry = current_user.entries.build(entry_params)
+
+    if @entry.save
+      redirect_to entries_path, notice: "Entry created successfully!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def entry_params
+    params.require(:entry).permit(:title, :body)
+  end
 end
