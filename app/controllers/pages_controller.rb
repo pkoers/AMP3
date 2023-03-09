153c80182958a760cbcc1ff084c60b6bb77f1f8e
Patrick Koers <pkoers75@gmail.com>
2023-03-09 13:14:01 +0100

    Moved the email all function to /pages/post

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    # command to send emails via mailtrap
+  end
+
+  def post
+  # command to send emails via mailtrap
     if current_user.present?
       # SendMailer.send_email(current_user).deliver_now
       users = Entry.pluck(:email)
@@ -10,5 +13,6 @@ class PagesController < ApplicationController
         SendMailer.send_email(user).deliver_now
       end
     end
+    redirect_to root_path
   end
 end
