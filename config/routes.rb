5c9b5616a11e953c09dcc569b7aa30ad082300d3
Patrick Koers <pkoers75@gmail.com>
2023-03-09 13:16:05 +0100

    moved the email all function to /post iso /pages/post

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  get '/pages/post', to: 'pages#post'
+  get '/post', to: 'pages#post'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
